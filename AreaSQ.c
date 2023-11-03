197c7f4c54061822433e930c232a7e7cc35fdcd5
Durgesh Shukla <124335520+durgeshshukla18@users.noreply.github.com>
2023-11-03 11:37:03 +0530

    Add files via upload

---
+++ AreaSQ.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("Enter the value of side of square:\n");
+	scanf("%d",&a);
+	printf("Area of square is:%d",a*a);
+	printf("\nPerimeter of square is:%d",4*a);
+}
---
+++ MathOP.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	printf("\nAddition of a and b is:%d",a+b);
+	printf("\nSubstraction of a and b is:%d",a-b);
+	printf("\nMultiplication of a and b is:%d",a*b);
+	printf("\nDivision of a and b is:%d",a/b);
+	printf("\nModulus of a and b is:%d",a%b);
+}
