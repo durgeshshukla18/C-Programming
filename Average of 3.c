a8e9033514983fb83e9df47f24937dfb4ec5011f
Durgesh Shukla <124335520+durgeshshukla18@users.noreply.github.com>
2023-11-04 01:37:10 +0530

    Add files via upload

---
+++ Average of 3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	printf("\nEnter the value of b:");
+	scanf("%d",&b);
+	printf("\nEnter the value of c:");
+	scanf("%d",&c);
+	printf("\nThe average value of a,b and c is:%d",(a+b+c)/3);
+	return 0;
+}
